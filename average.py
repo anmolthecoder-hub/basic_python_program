1b56235374c3d1dfb9b62526f53f08f9a7694bad
Satyam Anmol <221042144+anmolthecoder-hub@users.noreply.github.com>
2025-07-16 19:40:59 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+# find the averave of two number
+a=float(input("enter the first number"))
+b=float(input("enter the second number:"))
+
+average_value=0
+average_value=(a+b)/2
+print(f"average_value={average_value}")
---
+++ dataType.py
@@ -0,0 +1,4 @@
+a=30
+b="satyam"
+c=3.12
+print(f"a={a} ,b={b},c={c}")
