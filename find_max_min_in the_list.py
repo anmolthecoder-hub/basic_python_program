df047ec9f06970d5d73128c15d073e95ad4e3a74
Satyam Anmol <221042144+anmolthecoder-hub@users.noreply.github.com>
2025-07-17 13:18:56 +0530

    Add files via upload

---
+++ find_max_min_in the_list.py
@@ -0,0 +1,9 @@
+#find_max_min_in the_list
+# This code finds the maximum and minimum values in a list of numbers
+num=[23,45,67,89,12,34,56,78,90]
+
+for i in range(len(num)):
+    min_num=min(num)
+    max_num=max(num)
+print("maxmium number in the list=",max_num)
+print("minimum value of the list=",min_num)    
